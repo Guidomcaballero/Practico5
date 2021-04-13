2ac7ba01488da6b7dc192feabcc288c045b0bd6b
Usuario <Usuario@LSBTV3TIENDAB>
2021-04-13 19:52:56 -0300

    Resolucion ejercicio 1.

--- src/caballero/practico5/Arreglo.java
+++ src/caballero/practico5/Arreglo.java
@@ -18,7 +18,19 @@ public class Arreglo {
         }
         return aux;
     }
-    
-    
+    public static int buscarMayor(int bidimensional[][]){
+        int aux = 0;
+        for (int i=0;i<bidimensional.length;i++){
+            for (int j=0;j<bidimensional[i].length;i++){
+                if(aux<bidimensional[i][j])
+                    aux = bidimensional[i][j];
+            }
+        }
+        return aux;
+    }
+    public static int cuentaVocales(String palabra){
+        
+        return 0;
+    }
 }
 
